8b6ab1cb0cafe629b17a607af705a985decf6030
turczerb <turczerb@gmail.com>
2021-11-03 11:05:30 +0100

    :tada:

---
+++ index.js
@@ -0,0 +1,34 @@
+function readNews() {
+    fetch("rss.json")
+    .then((r) => r.json())
+    .then(renderNews);
+}
+
+function renderNews(news) {
+    let items= news.items;
+    for (let item of items) {
+        renderItem(item);
+
+}
+}
+
+function renderItem(item) {
+    let box = document
+    .createElement('div');
+
+    let title = document
+    .createElement('h1');
+
+    let desc = document
+    .createElement('p');
+
+    title.innerText = item.title;
+
+    desc.innerText = item.summary;
+
+    box.append(title, desc);
+    document.getElementById('rss').append(box);
+
+}
+
+window.addEventListener('load', readNews);
